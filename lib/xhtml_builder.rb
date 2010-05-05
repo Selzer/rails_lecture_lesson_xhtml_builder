086929e0c2d279677e103d15c646ee6a8d64c876
Tobias Selzer <toselzer@MacD.local>
2010-05-05 23:03:21 +0200

    update xhtml_builder

--- lib/xhtml_builder.rb
+++ lib/xhtml_builder.rb
@@ -1,29 +1,24 @@
 #
 #  xhtml_builder.rb
-#  
-#
-#  Created by Tobias Selzer on 26.04.10.
-
 
 class XHTMLBuilder
-	attr_accessor :content
+  attr_accessor :content
   
-	def initialize
-		@content = ""
-	end
+  def initialize
+	  @content = ""
+  end
 	
 	def method_missing(sym, *args, &block) 
     @content << "<#{sym}>\n"
 			
-		if block_given? then yield
-		else
-			unless (args[0].nil?)
-  			@content << args[0] << "\n"
-			end
-		end
+    if block_given? then yield
+	  else
+		  unless (args[0].nil?)
+  		  @content << args[0] << "\n"
+		  end
+	  end
     @content << "</#{sym}>\n"
   end	
-	
 end
 
 
